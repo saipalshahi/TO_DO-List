5efb8434d767cb53f1a587a80862a8451728ce8b
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-07-09 08:28:32 +0545

    second commit

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,13 +2,16 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { fetchUsers } from "../../api/Api";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../../redux_storage/userSlice";
 
 // make a function (fileName)
 const Homepage = () => {
   // make a dispatch
   const dispatch = useDispatch();
+
+  // selector (select from storage)  -----> list of users
+  const users = useSelector((state) => state.users.users);
 
   //automatic user fetch  {  useState}
   // []  is dependency list, when should it run
@@ -28,10 +31,59 @@ const Homepage = () => {
 
   return (
     <>
-      <h1 className="text-center">This is homepage</h1>
-      <Link to={"/login"} className="btn btn-outline-danger">
-        Logout
-      </Link>
+      <table className="table">
+        <thead className="table-primary">
+          <tr>
+            <th>ID</th>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>Doe</td>
+            <td>JohnDoe</td>
+            <td>john.doe@gmail.com</td>
+            <td>
+              <div className="btn-group ">
+                <button className="btn bg-success"> Edit</button>
+                <button className="btn bg-danger">Delete</button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>John</td>
+            <td>Doe</td>
+            <td>JohnDoe</td>
+            <td>john.doe@gmail.com</td>
+            <td>
+              <div className="btn-group ">
+                <button className="btn bg-success"> Edit</button>
+                <button className="btn bg-danger">Delete</button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>Doe</td>
+            <td>JohnDoe</td>
+            <td>john.doe@gmail.com</td>
+            <td>
+              <div className="btn-group ">
+                <button className="btn bg-success"> Edit</button>
+                <button className="btn bg-danger">Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
